6ed61d12f1c25ba2b63581078cf522fb2f56ced7
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2026-01-27 21:01:56 +0530

    added javascipt problems

--- striver-problems/basicMathProblems/checkPrime.js
+++ striver-problems/basicMathProblems/checkPrime.js
@@ -1,7 +1,8 @@
 function isPrime(n) {
   if (n <= 1) return false;
-  if(n ===2 ) return true;
-  if(n % 2 === 0) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  
   for (let i = 3; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
       return false;
