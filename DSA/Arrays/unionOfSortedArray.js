f64e99b08555aa54eb8dad36f7154e02468470e6
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2026-02-16 08:20:59 +0530

    using two pointer

--- DSA/Arrays/unionOfSortedArray.js
+++ DSA/Arrays/unionOfSortedArray.js
@@ -31,3 +31,54 @@ const arr4 = [2, 3, 4, 4, 5, 11, 12],
 
 console.log(unionOfSortedArray(arr1, n1, arr2, n2));
 console.log(unionOfSortedArray(arr3, n3, arr4, n4));
+
+///using Two pointer
+function unionOfSortedArrPointer(arr1, m, arr2, n) {
+  let unionArr = [];
+
+  let i = 0,
+    j = 0;
+
+  while (i < m && j < n) {
+    if (arr1[i] < arr2[j]) {
+      if (unionArr.length === 0 || unionArr[unionArr.length - 1] !== arr1[i]) {
+        unionArr.push(arr1[i]);
+        i++;
+      }
+    } else if (arr2[j] < arr1[i]) {
+      if (unionArr.length === 0 || unionArr[unionArr.length - 1] !== arr2[j]) {
+        unionArr.push(arr2[j]);
+        j++;
+      }
+    } else {
+      if (unionArr.length === 0 || unionArr[unionArr.length - 1] !== arr1[i]) {
+        unionArr.push(arr1[i]);
+        i++;
+        j++;
+      }
+    }
+  }
+
+  while (i < m) {
+    if (unionArr.length === 0 || unionArr[unionArr.length - 1] !== arr1[i]) {
+      unionArr.push(arr1[i]);
+      i++;
+    }
+  }
+
+  while (j < n) {
+    if (unionArr.length === 0 || unionArr[unionArr.length - 1] !== arr2[j]) {
+      unionArr.push(arr2[j]);
+      j++;
+    }
+  }
+
+  return unionArr;
+}
+
+const arr5 = [1, 2, 3],
+  m = arr5.length;
+const arr6 = [2, 3, 4],
+  n = arr6.length;
+
+console.log(unionOfSortedArrPointer(arr5, m, arr6, n));
