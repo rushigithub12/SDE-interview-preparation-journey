a0f53da9cb864d9d14c8afbbd82f724d519a43e4
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2026-02-24 09:51:26 +0530

    Update checkIfSubset.js

--- geeksForGeeks/hashingProblems/checkIfSubset.js
+++ geeksForGeeks/hashingProblems/checkIfSubset.js
@@ -23,7 +23,6 @@ const query2 = [19, 5, 3];
 console.log(checkIfSubset(arr1, query1));
 console.log(checkIfSubset(arr2, query2));
 
-
 //using hashSet
 function checkIfSubset1(a, b) {
   const hashSet = new Set(a);
@@ -40,24 +39,26 @@ console.log(checkIfSubset1(arr1, query1));
 console.log(checkIfSubset1(arr2, query2));
 
 //using two pointer
-function checkIfSubset2(a, b){
-    a.sort((x, y) => x - y);
-    b.sort((x, y) => x - y);
+function checkIfSubset2(a, b) {
+  a.sort((x, y) => x - y);
+  b.sort((x, y) => x - y);
 
-    let i=0, j=0;
-    let m = a.length, n = b.length;
+  let i = 0,
+    j = 0;
+  let m = a.length,
+    n = b.length;
 
-    while(i < m && j < n){
-        if(a[i] < b[j]){
-            i++;
-        }else if(a[i] === b[j]){
-            i++;
-            j++;
-        }else {
-            return false;
-        }
+  while (i < m && j < n) {
+    if (a[i] < b[j]) {
+      i++;
+    } else if (a[i] === b[j]) {
+      i++;
+      j++;
+    } else {
+      return false;
     }
-    return (j === n)
+  }
+  return j === n;
 }
 
 console.log(checkIfSubset2(arr1, query1));
