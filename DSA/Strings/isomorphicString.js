ff089f665a24f0d496871db5204d5cea2a612a60
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2026-03-16 09:37:19 +0530

    string problems

--- DSA/Strings/isomorphicString.js
+++ DSA/Strings/isomorphicString.js
@@ -29,22 +29,22 @@ const t1 = "bar";
 console.log(isomorphicString(s, t));
 console.log(isomorphicString(s1, t1));
 
-
 ///using array hash space complexity O(1)
-function isomorphicString1(s, t){
-    if(s.length !== t.length) return false;
+function isomorphicString1(s, t) {
+  if (s.length !== t.length) return false;
 
-    let hash1 = new Array(256).fill(-1);
-    let hash2 = new Array(256).fill(-1);
+  let hash1 = new Array(256).fill(-1);
+  let hash2 = new Array(256).fill(-1);
 
-    for(let i=0; i<s.length; i++){
-        if(hash1[s.charCodeAt(i)] !== hash2[t.charCodeAt(i)]){
-            return false;
-        }
-        hash1[s.charCodeAt(i)] = i + 1;
-        hash2[t.charCodeAt(i)] = i + 1;
+  for (let i = 0; i < s.length; i++) {
+    if (hash1[s.charCodeAt(i)] !== hash2[t.charCodeAt(i)]) {
+      return false;
     }
-    return true;
+    hash1[s.charCodeAt(i)] = i + 1;
+    hash2[t.charCodeAt(i)] = i + 1;
+  }
+  console.log(hash1)
+  return true;
 }
 
-console.log(isomorphicString1(s, t))
+console.log(isomorphicString1(s, t));
