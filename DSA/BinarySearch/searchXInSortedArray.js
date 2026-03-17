7d6434f1ba9f46e03db4cfa2fc8612973fba88ba
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2026-03-17 20:02:50 +0530

    two pointer string problems

--- DSA/BinarySearch/searchXInSortedArray.js
+++ DSA/BinarySearch/searchXInSortedArray.js
@@ -14,3 +14,20 @@ const arr = [3, 4, 6, 7, 9, 12, 16, 17],
   n = arr.length;
 
 console.log(searchXInSortedArray(arr, 6));
+
+///using recursion
+function searchXInSortedArray1(arr, low, high, target) {
+  if (low > high) return;
+
+  let mid = Math.floor((low + high) / 2);
+  if (target === arr[mid]) return mid;
+  else if (target > arr[mid]) {
+    return searchXInSortedArray1(arr, mid + 1, high, target);
+  } else {
+    return searchXInSortedArray1(arr, low, mid - 1, target);
+  }
+}
+const low = 0,
+  high = arr.length - 1;
+
+console.log(searchXInSortedArray1(arr, low, high, 6));
