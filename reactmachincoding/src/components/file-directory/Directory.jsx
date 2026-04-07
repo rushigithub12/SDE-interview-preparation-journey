2dcb9b499498bae214c46d3ba698f0202ac2c52c
rushigitpractice98 <rushikeshkamble9256@gmail.com>
2026-04-07 23:31:25 +0530

    added onRename, onDelete, onToggle functionality

--- reactmachincoding/src/components/file-directory/Directory.jsx
+++ reactmachincoding/src/components/file-directory/Directory.jsx
@@ -1,30 +1,21 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import FileDirectory from "./FileDirectory";
+import fileData from "./file-data.json";
 import "./Directory.css";
 
+//helper
+const addsTotree = (tree) => {
+  return tree.map((node) => ({
+    ...node,
+    id: uuidv4(),
+    isOpen: node.type === "directory" ? (node.isOpen ?? false) : undefined,
+    children: node.children ? addsTotree(node.children) : [],
+  }));
+};
+
 const Directory = () => {
-  const [directory, setDirectory] = useState([
-    {
-      id: uuidv4(),
-      name: "src",
-      type: "directory",
-      isOpen: true,
-      children: [
-        { id: uuidv4(), name: "index.js", type: "file", children: [] },
-        {
-          id: uuidv4(),
-          name: "components",
-          type: "directory",
-          isOpen: true,
-          children: [
-            { id: uuidv4(), name: "Button.js", type: "file", children: [] },
-          ],
-        },
-      ],
-    },
-    { id: uuidv4(), name: "package.json", type: "file", children: [] },
-  ]);
+  const [directory, setDirectory] = useState(addsTotree(fileData));
 
   const updateTree = (tree, id, callback) => {
     return tree.map((node) => {
@@ -45,11 +36,13 @@ const Directory = () => {
 
   const createNode = (parentId, type) => {
     const name = prompt(`Enter ${type} name`);
+    if(!name) return
 
     const newNode = {
       id: uuidv4(),
       name,
       type,
+      isOpen: type === "directory" ? true : false,
       children: type === "directory" ? [] : [],
     };
 
@@ -61,14 +54,67 @@ const Directory = () => {
     );
   };
 
+  const onRename = (id) => {
+    const newName = prompt("Enter name");
+    if (!newName) return;
+
+    setDirectory((prev) =>
+      updateTree(prev, id, (node) => ({
+        ...node,
+        name: newName,
+      })),
+    );
+  };
+
+  const onDelete = (id) => {
+    const deleteNode = (tree) => {
+      return tree
+        .filter((node) => node.id !== id)
+        .map((node) => ({
+          ...node,
+          children: node.children ? deleteNode(node.children) : [],
+        }));
+    };
+    setDirectory((prev) => deleteNode(prev));
+  };
+
+  const handleToggle = (id) => {
+    const updateTree = (tree) => {
+      return tree.map((node) => {
+        if (node.id === id) {
+          return {
+            ...node,
+            isOpen: node.type === "directory" ? !node.isOpen : false,
+          };
+        }
+        if (node.children) {
+          return {
+            ...node,
+            children: node.children ? updateTree(node.children) : [],
+          };
+        }
+        return node;
+      });
+    };
+
+    setDirectory((prev) => updateTree(prev));
+  };
+
   return (
-    <div>
+    <div className="directory-container">
       <header>
         <h1>File Explorer</h1>
       </header>
       <div>
         {directory?.map((dir, ind) => (
-          <FileDirectory key={dir.id} fileData={dir} onCreate={createNode} />
+          <FileDirectory
+            key={dir.id}
+            fileData={dir}
+            onCreate={createNode}
+            onRename={onRename}
+            onDelete={onDelete}
+            onToggle={handleToggle}
+          />
         ))}
       </div>
     </div>
--- reactmachincoding/src/components/file-directory/FileDirectory.jsx
+++ reactmachincoding/src/components/file-directory/FileDirectory.jsx
@@ -1,33 +1,56 @@
 import React, { useState } from "react";
 import "./Directory.css";
 
-const FileDirectory = ({ fileData, onCreate }) => {
-  const [showChildren, setShowChildren] = useState(false);
-
-  const handleShowChild = () => {
-    setShowChildren(!showChildren);
-  };
-
+const FileDirectory = ({
+  fileData,
+  onCreate,
+  onRename,
+  onDelete,
+  onToggle,
+}) => {
   return (
-    <div>
-      <h5>
-        {fileData?.type === "directory" ? (showChildren ? "📂" : "📁") : "📄"}
-        <span className="directory-folder-name" onClick={handleShowChild}>
+    <div className="filedirectory-container">
+      <div>
+        <span className="filedirectory-icon">
+          {fileData?.type === "directory"
+            ? fileData.isOpen
+              ? "📂"
+              : "📁"
+            : "📄"}
+        </span>
+        <span
+          className="directory-folder-name"
+          onClick={() => onToggle(fileData.id)}
+        >
           {fileData.name}
         </span>
-        {fileData?.type === "directory" && (
-          <>
-            <button onClick={() => onCreate(fileData.id, "file")}>+File</button>
-            <button onClick={() => onCreate(fileData.id, "directory")}>
-              +Folder
-            </button>
-          </>
-        )}
-      </h5>
+        <span className="filedirectory-action">
+          <button onClick={() => onRename(fileData.id)}>✏️</button>
+          <button onClick={() => onDelete(fileData.id)}>❌</button>
+
+          {fileData?.type === "directory" && (
+            <>
+              <button onClick={() => onCreate(fileData.id, "file")}>
+                +File
+              </button>
+              <button onClick={() => onCreate(fileData.id, "directory")}>
+                +Folder
+              </button>
+            </>
+          )}
+        </span>
+      </div>
       <div>
-        {showChildren &&
+        {fileData.isOpen &&
           fileData?.children?.map((files, ind) => (
-            <FileDirectory key={files.id} fileData={files} onCreate={onCreate} />
+            <FileDirectory
+              key={files.id}
+              fileData={files}
+              onCreate={onCreate}
+              onRename={onRename}
+              onDelete={onDelete}
+              onToggle={onToggle}
+            />
           ))}
       </div>
     </div>
